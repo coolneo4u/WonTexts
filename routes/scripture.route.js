d9872a317f7cad61fc815f2baf61919c50ddb4ed
Taehyung Cho <tcho@amazon.com>
2019-03-07 08:20:53 -0800

    preparing auth on frontend

--- routes/scripture.route.js
+++ routes/scripture.route.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const scriptureController = require('../controllers/scripture.controller')
+const auth = require('../auth')
 
-router.post('/create', scriptureController.create)
-router.get('/get', scriptureController.get)
-router.put('/update', scriptureController.update)
-router.delete('/delete', scriptureController.delete)
-router.get('/indexes', scriptureController.indexes)
+router.post('/create', auth.required, scriptureController.create)
+router.get('/get', auth.optional, scriptureController.get)
+router.put('/update', auth.required, scriptureController.update)
+router.delete('/delete', auth.required, scriptureController.delete)
+router.get('/indexes', auth.optional, scriptureController.indexes)
 
 module.exports = router
